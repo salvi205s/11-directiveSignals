1891cc57663c05f2acf7b403c575f905806d9ee7
salvi205s <salvi205s@gmail.com>
2023-11-13 11:15:30 +0100

    Fin seccion 23: Directivas personalizadas de Angular

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomLabelDirective } from './directives/custom-label.directive';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    CustomLabelDirective
+  ],
+  exports: [CustomLabelDirective]
+})
+export class SharedModule { }
